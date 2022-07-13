36d064c4f7b0c33a84ed362b3cda487ad1af20ee
Jelle VdP <jellevandepoel@gmail.com>
2022-07-13 11:54:56 +0200

    added basic navigation

--- views/includes/header.php
+++ views/includes/header.php
@@ -7,4 +7,13 @@
     <title>CRUD application</title>
 </head>
 <body>
+    <header>
+        <h1>CRUD application</h1>
+        <nav>
+            <ul>
+                <li><a href="?page=students">Students</a></li>
+                <li><a href="?page=groups">Classes</a></li>
+                <li><a href="?page=coaches">Coaches</a></li>
+            </ul>
+        </nav>
     <main>
