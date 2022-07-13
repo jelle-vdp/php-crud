5c281b273eb147d0900e8919fac1122e3b9e567e
Jelle VdP <jellevandepoel@gmail.com>
2022-07-13 11:09:23 +0200

    added additional coach files

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-require 'models/Students.php';
-require 'models/Groups.php';
+    require 'models/Student.php';
+    require 'models/Group.php';
+    require 'models/Coach.php';
 
-require 'controllers/StudentsController.php';
-require 'controllers/GroupsController.php';
+    require 'controllers/StudentsController.php';
+    require 'controllers/GroupsController.php';
+    require 'controllers/CoachesController.php';
 
+    $controller = new StudentsController();
+    if(isset($_GET['page']) && $_GET['page'] === 'groups') {
+        $controller = new GroupsController();
+    } else if(isset($_GET['page']) && $_GET['page'] === 'students') {
+        $controller = new StudentsController();
+    } else if(isset($_GET['page']) && $_GET['page'] === 'coaches') {
+        $controller = new CoachesController();
+    } 
 
-$controller = new StudentsController();
-if(isset($_GET['page']) && $_GET['page'] === 'groups') {
-    $controller = new GroupsController();
-}
-
-$controller->render($_GET, $_POST);
+    $controller->render($_GET, $_POST);
