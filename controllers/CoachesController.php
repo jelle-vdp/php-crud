601f6c33e65140a519cc705b9886e9adbd8a484a
Besart Elezi <besart.elezi@hotmail.com>
2022-07-14 11:19:37 +0200

    added DatabaseLoader to this controller

--- controllers/CoachesController.php
+++ controllers/CoachesController.php
@@ -2,6 +2,20 @@
 
 class CoachesController
 {
+    private DatabaseLoader $databaseLoader;
+
+    public function __construct() {
+        $this->databaseLoader = new DatabaseLoader();
+    }
+
+    public function getAllDataCoaches () {
+        $sqlAllDataCoaches = $this->databaseLoader->getConnection()->query("SELECT * FROM teacher_table");
+        $allDataCoaches = [];
+        while ($row = $sqlAllDataCoaches->fetch()){
+            $allDataCoaches[] = new Coach($row[0], $row[1], $row[2]);
+        }
+    }
+
     public function render($get, $post)
     {
         require("views/coachesView.php");
