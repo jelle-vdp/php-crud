5919359d8b97029b330120eae9cce706c5fad957
Besart Elezi <besart.elezi@hotmail.com>
2022-07-14 11:40:34 +0200

    renamed files from coaches to teachers

---
+++ controllers/TeacherController.php
@@ -0,0 +1,23 @@
+<?php
+
+class CoachesController
+{
+    private DatabaseLoader $databaseLoader;
+
+    public function __construct() {
+        $this->databaseLoader = new DatabaseLoader();
+    }
+
+    public function render($get, $post)
+    {
+        $sqlAllDataTeachers = $this->databaseLoader->getConnection()->query("SELECT * FROM teacher_table");
+        $allDataTeachers = [];
+        while ($row = $sqlAllDataTeachers->fetch()){
+            $allDataTeachers[] = new Coach($row[0], $row[1], $row[2]);
+        }
+
+
+
+        require("views/coachesView.php");
+    }
+}
---
+++ views/teacherView.php
@@ -0,0 +1,19 @@
+<?php
+
+require("views/includes/header.php");
+
+foreach ($allDataTeachers as $dataTeacher) {
+    $teacherName = ucfirst($dataTeacher->getName());
+    $teacherID = $dataTeacher->getID();
+    $teacherEmail = $dataTeacher->getEmail();
+    echo $teacherName . " has Coach ID: " . $teacherID ."and their email-adress is: " . $teacherEmail . "<br>";
+}
+
+?>
+
+
+
+<h1>Teachers</h1>
+
+
+<?php require("views/includes/footer.php");
