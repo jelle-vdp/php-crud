1c81ea80a8a04d73c955201c0c2a4f33d5c33b8d
Jelle VdP <jellevandepoel@gmail.com>
2022-07-14 16:56:39 +0200

    added group view and controller

--- controllers/GroupsController.php
+++ controllers/GroupsController.php
@@ -1,8 +1,30 @@
 <?php
 
 class GroupsController {
+    private DatabaseLoader $databaseLoader;
+    private array $allGroups;
+
+    public function __construct(){
+        $this-> databaseLoader = new DatabaseLoader();
+    }
+
+    public function getAllDataGroups(){
+        $sqlAllDataGroups = $this->databaseLoader->getConnection()->query("SELECT * FROM group_table");
+        $allDataGroups = [];
+        while ($row = $sqlAllDataGroups->fetch()){
+            array_push($allDataGroups, new Group($row['id'], $row['name'], $row['location'], $row['teacher_id']));
+        }
+        $this->allGroups = $allDataGroups;
+    }
+
+    public function getAllGroups(){
+        return $this->allGroups;
+    }
+
     public function render($get, $post)
     {
+         $this->getAllDataGroups();
+
         require("views/groupsView.php");
     }
 }
--- views/groupsView.php
+++ views/groupsView.php
@@ -1,8 +1,30 @@
 <?php
 
-require("views/includes/header.php");
-require("views/includes/footer.php");
+    require("views/includes/header.php");
 
 ?>
 
 <h1>Classes</h1>
+
+<?php
+
+    if(count($this->allGroups) === 0){
+        echo "<p>No groups found</p>";
+    } else {
+        echo "<table><thead><tr><th>Name</th><th>Location</th><th>Teacher</th></tr></thead><tbody>";
+
+        foreach ($this->allGroups as $dataGroup) {
+            $groupName = ucfirst($dataGroup->getName());
+            $groupId = $dataGroup->getID();
+            $groupLocation = $dataGroup->getLocation();
+            $groupTeacherId = $dataGroup->getTeacherId();
+
+            echo "<tr><td>$groupName</td><td>$groupLocation</td><td>$groupTeacherId</td></tr>";
+        }
+
+        echo "</tbody></table>";
+    }
+
+    require("views/includes/footer.php");
+
+?>
