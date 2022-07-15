c1c68c64aff0100daf49ee24d7e3bf09a3e3fcc7
Besart Elezi <besart.elezi@hotmail.com>
2022-07-15 15:04:59 +0200

    added create teacher button to table

--- views/teacherView.php
+++ views/teacherView.php
@@ -2,6 +2,8 @@
 
 require("views/includes/header.php");
 ?>
+
+    <h1>Teachers</h1>
     <table>
         <tr>
             <th>ID</th>
@@ -23,8 +25,18 @@ require("views/includes/header.php");
 ?>
     </table>
 
+    </table>
 
-<h1>Teachers</h1>
+    <form action="?page=teachers" method="post">
+    <label for="teacher">Create a new teacher:</label><br>
+    <input type="text" name="name" placeholder="Teacher's name"><br>
+    <input type="text"  name="email" placeholder="teacher's email"><br>
+    <button name="method" type="submit" value="create">Create</button>
+
+    <?php
+    if (isset($_POST["name"])) {
+        var_dump($_POST);
+    }    ?>
 
 
 <?php require("views/includes/footer.php");
