e4ae1ebaea62c8625f977a901595b920427dd1af
Besart Elezi <besart.elezi@hotmail.com>
2022-07-17 16:01:17 +0200

    added delete function
    also did some light restructuring of the code

--- controllers/TeacherController.php
+++ controllers/TeacherController.php
@@ -11,13 +11,29 @@ class TeacherController
 
     public function render($get, $post)
     {
+        //Loads all the data of the teachers
         $this->getAllDataTeachers();
 
-        if ($post["method"] = "create" && isset($post['name']) && isset($post['email']) ) {
+        //if a button was pressed with the method 'create' and all values were set, creates a new Teacher with those values
+        if ($post["method"] = "create" && isset($post['name']) && isset($post['email'])) {
             $this->createNewTeacher($post);
             $this->getAllDataTeachers();
+            require("./views/teacherView.php");
         }
-        require("views/teacherView.php");
+
+        else if (isset($post['delete']) && !isset($post['confirm'])) {
+            require("./views/deleteTeacherView.php");
+        }
+
+        else if (isset($post['delete']) && $post['confirm'] === 'true') {
+            $this->deleteTeacher($post['delete']);
+            $this->getAllDataTeachers();
+            require("./views/teacherView.php");
+        }
+
+        else {
+            require("./views/teacherView.php");
+        }
     }
 
     public function getAllDataTeachers () {
@@ -33,6 +49,11 @@ class TeacherController
         $name = $post['name'];
         $email = $post['email'];
         $this->databaseLoader->getConnection()->query("INSERT INTO teacher_table VALUES ( id ,'$name', '$email')");
+    }
+
+    public function deleteTeacher($id)
+    {
+        $this->databaseLoader->getConnection()->query("DELETE FROM teacher_table WHERE id = $id");
     }
 }
 
